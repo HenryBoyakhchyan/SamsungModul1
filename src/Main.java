dc75cd1f87f24b6533fc3fbe5bab81a50867b173
henryboyakhchyan <henry.boyakhchyan@gmail.com>
2024-09-10 21:49:48 +0400

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        int a = 12;
+        for (int i = 1; i <= 5; i++) {
+            int c = a + i;
+            System.out.println("i = " + i + "\n c = " + c);
+        }
+    }
+}
